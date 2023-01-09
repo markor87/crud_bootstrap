de95a7529601d0f2a0a16d3032037eb69cb5d684
Marko <mradovanovic@msn.com>
2023-01-09 14:00:36 +0100

    commit

--- js/login_validation.js
+++ js/login_validation.js
@@ -1,31 +1,24 @@
-// Get the form element
-let form = document.getElementById("login-form");
+function validateForm() {
+    // Get the values of the email and password fields
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-// Add a submit event listener to the form
-form.addEventListener("submit", function(event) {
-    // Prevent the form from being submitted
-    event.preventDefault();
-
-    // Get the email and password input elements
-    let email = document.getElementById("emailInput");
-    let password = document.getElementById("passwordInput");
-
-    // Validate the email and password
-    if (!email.value) {
-        // Email is empty, display an error message
-        alert("Please enter your email.");
-    } else if (!password.value) {
-        // Password is empty, display an error message
-        alert("Please enter your password.");
-    } else {
-        // Email and password are valid, submit the form
-        form.submit();
+    // Validate the email field
+    if (email === "") {
+        // If the email field is empty, display an error message
+        document.getElementById("emailHelp").style.display = "block";
+        document.getElementById("emailHelp").innerHTML = "Please enter your email.";
+        return false;
     }
-});
 
-// Add a registration button
-let button = document.getElementById("register-button");
-button.addEventListener("click", function(event) {
-    // Open the registration form in a new window
-    window.open("/register.php", "_blank", "height=500, width=500");
-});
+    // Validate the password field
+    if (password === "") {
+        // If the password field is empty, display an error message
+        document.getElementById("passwordHelp").style.display = "block";
+        document.getElementById("passwordHelp").innerHTML = "Please enter your password.";
+        return false;
+    }
+
+    // If the email and password fields are not empty, submit the form
+    return false;
+}
