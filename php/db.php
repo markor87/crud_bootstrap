a1d33235d0100d6886c7b334a81ca6bedd03c3a2
Marko <mradovanovic@msn.com>
2023-01-10 13:51:33 +0100

    commit

--- php/db.php
+++ php/db.php
@@ -1,10 +1,10 @@
 <?php
 
 // Connection parameters
-$host = 'localhost';
-$user = 'username';
-$password = 'password';
-$dbname = 'database_name';
+$host = '10.15.32.49';
+$user = 'marko.radovanovic';
+$password = 'LoneDruid1987';
+$dbname = 'izvrsioci';
 
 // Establish connection
 $conn = mysqli_connect($host, $user, $password, $dbname);
@@ -13,28 +13,5 @@ $conn = mysqli_connect($host, $user, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// Prepared statement
-$query = "SELECT * FROM users WHERE username = ? AND password = ?";
-$stmt = mysqli_prepare($conn, $query);
-
-// Bind parameters
-$username = "my_username";
-$password = "my_password";
-mysqli_stmt_bind_param($stmt, "ss", $username, $password);
-
-// Execute statement
-mysqli_stmt_execute($stmt);
-
-// Get result
-$result = mysqli_stmt_get_result($stmt);
-
-// Fetch rows
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['username'] . " " . $row['email'] . "<br>";
-}
-
-// Close connection
-mysqli_close($conn);
 
 ?>
